d38b7812c9d2c5905a44b62130d771c6a7315d8b
Shevani Arun <shevaniarun.1907043@srec.ac.in>
2022-10-10 18:52:25 +0530

    added new function for addition

--- acro.py
+++ acro.py
@@ -1,4 +1,12 @@
-a = 2
-b = 3
-c = a+b
-print(c)
+def add_numbers(a,b):
+    sum_of_two_no = a+b
+    return sum_of_two_no
+
+# a = 2
+# b = 3
+# c = a+b
+# print(c)
+
+add = add_numbers(10,20)
+print(add)
+
